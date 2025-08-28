c714128b82ff8b51f6b0278852fcf81f388ecce4
Alexey Chernov <leshachernov200@mail.ru>
2025-08-28 14:05:42 +1000

    otkat naxui etot auth

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,12 @@
-import { useState } from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import BoardPage from '../pages/boardpage/BoardPage'
-import LoginPage from '../pages/loginpage/LoginPage'
 import Pomodoro from '../pages/pomodoro/Pomodoro'
 import { Layout } from './layout/Layout'
 
 export default function App() {
-	const [isAuth, setIsAuth] = useState(false)
 	return (
 		<Routes>
-			<Route path='/login' element={<LoginPage />} />
-			<Route
-				path='/'
-				element={isAuth ? <Layout /> : <Navigate to='/login' replace />}
-			>
+			<Route path='/' element={<Layout />}>
 				<Route path='/board' element={<BoardPage />} />
 				<Route path='/pomodoro' element={<Pomodoro />} />
 			</Route>
--- src/ui/label/Label.tsx
+++
@@ -1,5 +0,0 @@
-import type { LabelProps } from '../../app/@types/types'
-
-export const Label = ({title, htmlFor}:LabelProps) =>{
-  return <label style={{color: 'blueviolet', fontSize: '20px'}} htmlFor={htmlFor}>{title}</label>
-}
